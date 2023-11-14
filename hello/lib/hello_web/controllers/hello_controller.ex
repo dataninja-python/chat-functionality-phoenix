66c3a098023052abe36c44349d0a81247a582c47
dataninja-python <the.coders.dao@gmail.com>
2023-11-14 15:48:49 -0500

    hello app updated

--- hello/lib/hello_web/controllers/hello_controller.ex
+++ hello/lib/hello_web/controllers/hello_controller.ex
@@ -1,6 +1,10 @@
 defmodule HelloWeb.HelloController do
   use HelloWeb, :controller
 
+  def show(conn, %{"messenger" => messenger} = params) do
+    render(conn, :show, messenger: messenger)
+  end
+
   def index(conn, _params) do
     render(conn, :index)
   end
