5690be2c447523580a9d48d514567d1712c53f4d
dataninja-python <the.coders.dao@gmail.com>
2023-11-14 12:57:43 -0500

    built all the way through to the point where using HEEX template for home_live

--- chat/lib/chat_web/router.ex
+++ chat/lib/chat_web/router.ex
@@ -17,7 +17,7 @@ defmodule ChatWeb.Router do
   scope "/", ChatWeb do
     pipe_through :browser
 
-    get "/", PageController, :home
+    live "/", HomeLive, :home
   end
 
   # Other scopes may use custom stacks.
