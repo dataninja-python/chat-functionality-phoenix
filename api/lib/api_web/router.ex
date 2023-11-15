672d461aaf36cd94c0af633055bdf1c44fc05beb
dataninja-python <the.coders.dao@gmail.com>
2023-11-15 14:49:10 -0500

    updated project files

--- api/lib/api_web/router.ex
+++ api/lib/api_web/router.ex
@@ -1,11 +1,11 @@
-defmodule HelloWeb.Router do
-  use HelloWeb, :router
+defmodule ApiWeb.Router do
+  use ApiWeb, :router
 
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
     plug :fetch_live_flash
-    plug :put_root_layout, html: {HelloWeb.Layouts, :root}
+    plug :put_root_layout, html: {ApiWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
   end
@@ -14,21 +14,19 @@ defmodule HelloWeb.Router do
     plug :accepts, ["json"]
   end
 
-  scope "/", HelloWeb do
+  scope "/", ApiWeb do
     pipe_through :browser
 
     get "/", PageController, :home
-    get "/hello", HelloController, :index
-    get "/hello/:messenger", HelloController, :show
   end
 
   # Other scopes may use custom stacks.
-  # scope "/api", HelloWeb do
+  # scope "/api", ApiWeb do
   #   pipe_through :api
   # end
 
   # Enable LiveDashboard and Swoosh mailbox preview in development
-  if Application.compile_env(:hello, :dev_routes) do
+  if Application.compile_env(:api, :dev_routes) do
     # If you want to use the LiveDashboard in production, you should put
     # it behind authentication and allow only admins to access it.
     # If your application does not have an admins-only section yet,
@@ -39,7 +37,7 @@ defmodule HelloWeb.Router do
     scope "/dev" do
       pipe_through :browser
 
-      live_dashboard "/dashboard", metrics: HelloWeb.Telemetry
+      live_dashboard "/dashboard", metrics: ApiWeb.Telemetry
       forward "/mailbox", Plug.Swoosh.MailboxPreview
     end
   end
